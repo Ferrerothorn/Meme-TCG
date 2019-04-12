d6f53814b9f1c77fdea3fc0d92baf074c62c45a3
Steve Dolman <stephendolman2112@gmail.com>
2019-04-12 16:30:14 +0100

    No need for card logging statemment

--- src/cards/Heal.java
+++ src/cards/Heal.java
@@ -12,8 +12,5 @@ public class Heal extends Card {
 	@Override
 	public void onentry(Player self, Player opponent) {
 		self.lifeTotal+=4;
-	//	System.out.println(self.getName() + " casts Heal for 4 lifegain! (" + self.getLife() + ")-(" + opponent.getLife() + ")");
-	}
-	
-	
+	}	
 }
