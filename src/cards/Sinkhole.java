ba8dea12809d5d041aaa0589e022dc6e2fecf3cb
Steve Dolman <stephendolman2112@gmail.com>
2019-04-12 16:33:01 +0100

    Buff Sinkhole. Win if 2 cards or less are left in your deck. If not, force a discard, if you can't, draw.

--- src/cards/Sinkhole.java
+++ src/cards/Sinkhole.java
@@ -4,18 +4,20 @@ import game.Player;
 
 public class Sinkhole extends Card {
 
-	
 	public Sinkhole() {
 		this.name = "Sinkhole";
 	}
-	
+
 	@Override
 	public void onentry(Player self, Player opponent) {
-		if(self.getDeck().size() == 0) {
+		if (self.getDeck().size() <= 2) {
 			opponent.lifeTotal = 0;
-		}
-		else {
-			self.draw();
+		} else {
+			if (opponent.getHand().size() > 1) {
+				opponent.randomDiscard();
+			} else {
+				self.draw();
+			}
 		}
 	}
 
