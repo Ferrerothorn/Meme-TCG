c34c7a829cc38b5df161a0823708fa952874eedb
Steve Dolman <stephendolman2112@gmail.com>
2019-04-12 16:29:18 +0100

    No need for logging statements

--- src/cards/Zap.java
+++ src/cards/Zap.java
@@ -12,7 +12,6 @@ public class Zap extends Card {
 	@Override
 	public void onentry(Player self, Player opponent) {
 		opponent.lifeTotal = opponent.lifeTotal-3;
-	//	System.out.println(self.getName() + " casts Zap on " + opponent.getName() + " for 3 damage! (" + self.getLife() + ")-(" + opponent.getLife() + ")");
 	}
 	
 	
