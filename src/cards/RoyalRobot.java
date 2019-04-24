603bd63c98692fec251bfffa3e142486b1869330
Steve Dolman <stephendolman2112@gmail.com>
2019-04-24 15:57:39 +0100

    New card: Royal Robot.

---
+++ src/cards/RoyalRobot.java
@@ -0,0 +1,24 @@
+package cards;
+
+import extraData.Card;
+import game.Player;
+
+public class RoyalRobot extends Card {
+
+	public RoyalRobot() {
+		this.name = "Royal Robot";
+	}
+
+	@Override
+	public void onentry(Player self, Player opponent) {
+		this.setCounters(2);
+	}
+
+	@Override
+	public void graveAbility(Player self, Player opponent) {
+		if (this.getCounters() > 0) {
+			this.takeCounter();
+			self.makePlay(opponent, false);
+		}
+	}
+}
