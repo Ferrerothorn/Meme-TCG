0b901185e6c9fa2cea3d700686c3fcfce08c6835
Steve Dolman <stephendolman2112@gmail.com>
2019-04-26 15:13:09 +0100

    Bugfix: Wrench would get stuck in strange loops.

--- src/cards/MightyWrench.java
+++ src/cards/MightyWrench.java
@@ -12,23 +12,18 @@ public class MightyWrench extends Card {
 	@Override
 	public void onentry(Player self, Player opponent) {
 		int wrenches = 0;
-		System.out.println("Entering Wronch");
 		for (Card c : self.grave) {
-			System.out.println(c.getName());
 			if (c.getCounters() > 0) {
 				wrenches++;
-				System.out.println("+1");
 			}
 		}
-		if (wrenches > 0) {
-			System.out.println("Wrenches");
-			while (opponent.getHand().size() > 0 && wrenches > 0) {
-				wrenches--;
-				opponent.randomDiscard();
-			}
-			while (wrenches > 0) {
-				self.draw();
-			}
+		while (opponent.getHand().size() > 0 && wrenches > 0) {
+			wrenches--;
+			opponent.randomDiscard();
+		}
+		while (wrenches > 0 && self.getDeck().size() > 0) {
+			self.draw();
+			wrenches--;
 		}
 	}
 
