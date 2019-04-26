c213b0c77e4dbd01fc850bb7790d8ca0c6b46a7c
Steve Dolman <stephendolman2112@gmail.com>
2019-04-26 15:56:16 +0100

    Buff: Corrupted blood was too easy to devolve into infinite games.

--- src/extraData/CorruptedBlood.java
+++ src/extraData/CorruptedBlood.java
@@ -6,19 +6,28 @@ import game.Player;
 
 public class CorruptedBlood extends Card {
 
-	
 	public CorruptedBlood() {
 		this.name = "Corrupted Blood";
 	}
-	
+
 	@Override
 	public void onentry(Player self, Player opponent) {
 
 		self.getDeck().add(new CorruptedBlood());
 		self.getDeck().add(new CorruptedBlood());
-		self.lifeTotal -= (self.cardCount(self.getDeck(), "Corrupted Blood")+1);
-		
+		self.lifeTotal -= (fibonacci((self.cardCount(self.getDeck(), "Corrupted Blood") / 2) + 1));
+
 		self.draw();
 		Collections.shuffle(self.getDeck());
-	}	
+	}
+
+	public static int fibonacci(int n) {
+		if (n <= 1) {
+			return n;
+
+		} else {
+			return fibonacci(n - 1) + fibonacci(n - 2);
+		}
+	}
+
 }
