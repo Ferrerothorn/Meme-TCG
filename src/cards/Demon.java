fb092be5f94d91e97a9ec2a11a80202be3aa9c22
Steve Dolman <stephendolman2112@gmail.com>
2019-04-26 15:13:36 +0100

    Bugfix: Demon shouldn't do anything unless explicitly played.

--- src/cards/Demon.java
+++ src/cards/Demon.java
@@ -11,11 +11,14 @@ public class Demon extends Card {
 
 	@Override
 	public void onentry(Player self, Player opponent) {
+		this.addCounter();
 		self.lifeTotal -=3;
 	}
 
 	@Override
 	public void graveAbility(Player self, Player opponent) {
+		if(this.getCounters()>0) {
 			opponent.lifeTotal -= 2;
+		}
 	}
 }
