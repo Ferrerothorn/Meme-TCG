0bd1e122be0847da68107cb9470203fc2211c027
Steve Dolman <stephendolman2112@gmail.com>
2019-04-26 15:56:35 +0100

    Bugfix: Tasty bread should have limited slices or else infinite games are too easy.

--- src/cards/TastyBread.java
+++ src/cards/TastyBread.java
@@ -8,19 +8,27 @@ import game.Player;
 
 public class TastyBread extends Card {
 
-	
 	public TastyBread() {
 		this.name = "Tasty Bread";
 	}
-	
+
 	@Override
 	public void onentry(Player self, Player opponent) {
-		self.lifeTotal+=3;
-	}	
-	
+		self.lifeTotal += 3;
+	}
+
 	@Override
 	public void graveAbility(Player self, Player opponent) {
-		self.getDeck().add(new Pigeon());
-		Collections.shuffle(self.getDeck());
+		if (this.getCounters() <= 10) {
+			this.addCounter();
+			self.getDeck().add(new Pigeon());
+			Collections.shuffle(self.getDeck());
+		}
+		this.addCounter();
+		if(this.getCounters()>=10) {
+			if(self.grave.remove(this)) {
+				self.rfg.add(this);
+			}
+		}
 	}
 }
