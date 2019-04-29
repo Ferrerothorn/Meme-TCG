6925b4877f6dff6aa4c878314acb43b2327e7092
Steve Dolman <stephendolman2112@gmail.com>
2019-04-29 15:59:55 +0100

    Performance: Refactor Error Message to fail faster.

--- src/extraData/ErrorMessage.java
+++ src/extraData/ErrorMessage.java
@@ -1,20 +1,13 @@
 package extraData;
 
-import game.Player;
-
 public class ErrorMessage extends Card {
 
 	String error = "";
-	
+
 	public ErrorMessage(String string) {
 		this.name = "Error Message";
 		error = string;
-	}
-
-	@Override
-	public void onentry(Player self, Player opponent) {
 		System.out.println("The config for " + error + " in the card pool index is spelled wrongly. Check it out!");
 		System.exit(0);
 	}
-
 }
