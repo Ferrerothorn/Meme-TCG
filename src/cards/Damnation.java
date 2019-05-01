dc817c18fd1d7b7ca8b8798aa4dc3094a36d92ca
Steve Dolman <stephendolman2112@gmail.com>
2019-05-01 15:47:36 +0100

    Nerf: Damnation should be dual-sided

--- src/cards/Damnation.java
+++ src/cards/Damnation.java
@@ -11,6 +11,7 @@ public class Damnation extends Card {
 
 	@Override
 	public void onentry(Player self, Player opponent) {
+		self.movePile(self.rfg, self.grave);
 		opponent.movePile(opponent.rfg, opponent.grave);
 		if(self.playsPerTurn<2) {
 		self.playsPerTurn = 2;
