f7d3ab77a2ad0b3dd5efaf494392ed033598cc86
Steve Dolman <stephendolman2112@gmail.com>
2019-05-23 13:52:13 +0100

    Buff: Checkmate is now stronger when it triggers.

--- src/cards/Checkmate.java
+++ src/cards/Checkmate.java
@@ -11,12 +11,21 @@ public class Checkmate extends Card {
 
 	@Override
 	public void onentry(Player self, Player opponent) {
-		if(opponent.getHand().size()==0) {
+		if (opponent.getHand().size() == 0) {
 			opponent.movePile(opponent.rfg, opponent.grave);
-			if(self.playsPerTurn<2) {
-			self.playsPerTurn = 2;
+			if (self.playsPerTurn < 2) {
+				self.playsPerTurn = 2;
+				this.addCounter();
 			}
+		} else {
+			opponent.lifeTotal -= 2;
 		}
-		opponent.lifeTotal -= 2;
+	}
+	
+	@Override
+	public void graveAbility(Player self, Player opponent) {
+		if(this.getCounters()>0) {
+			opponent.lifeTotal -= 2;
+		}
 	}
 }
