ad75b87954a20ac0d0aa12b22e653f2e28e96754
Steve Dolman <stephendolman2112@gmail.com>
2019-06-05 16:37:23 +0100

    New cards

---
+++ src/cards/DoublingZap.java
@@ -0,0 +1,34 @@
+package cards;
+
+import java.util.Collections;
+
+import extraData.Card;
+import game.Player;
+
+public class DoublingZap extends Card {
+
+	
+	public DoublingZap() {
+		this.name = "Doubling Zap";
+	}
+	
+	@Override
+	public void onentry(Player self, Player opponent) {
+		opponent.lifeTotal = opponent.lifeTotal-2;
+		Zap newZap = new Zap();
+		newZap.setName("Copied " + newZap.getName());
+		self.getHand().add(newZap);
+		Collections.shuffle(self.getHand());
+	}	
+	
+	
+	@Override
+	public void afterResolving(Player self, Player opponent) {
+		for (Card c: self.grave) {
+			if (c.getName().equals("Zap Magnifier") && c.getCounters()>0) {
+				opponent.lifeTotal-= c.getCounters();
+				c.takeCounter();
+			}
+		}
+	}	
+}
