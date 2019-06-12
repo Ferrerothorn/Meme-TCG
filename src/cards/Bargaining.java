d42897e6cc62d20d32a7edf662a1720217ba9015
Steve Dolman <stephendolman2112@gmail.com>
2019-06-12 14:33:10 +0100

    New cards and lists

---
+++ src/cards/Bargaining.java
@@ -0,0 +1,26 @@
+package cards;
+
+import extraData.Card;
+import game.Player;
+
+public class Bargaining extends Card {
+
+	public Bargaining() {
+		this.name = "Bargaining";
+	}
+
+	@Override
+	public void onentry(Player self, Player opponent) {
+		if (self.getHand().size() >= 1) {
+			self.randomDiscard();
+			self.playsPerTurn++;
+		}
+		if (self.playsPerTurn > 2) {
+			self.playsPerTurn++;
+		}
+	}
+
+	@Override
+	public void graveAbility(Player self, Player opponent) {
+	}
+}
---
+++ src/cards/Genome.java
@@ -0,0 +1,23 @@
+package cards;
+
+import extraData.Card;
+import game.Player;
+
+public class Genome extends Card {
+
+	public Genome() {
+		this.name = "Genome";
+	}
+
+	@Override
+	public void onentry(Player self, Player opponent) {
+		opponent.randomDiscard();
+	}
+
+	@Override
+	public void graveAbility(Player self, Player opponent) {
+		if (opponent.getHand().size() <= 2) {
+			opponent.lifeTotal--;
+		}
+	}
+}
