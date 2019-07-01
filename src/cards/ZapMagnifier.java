dcf2614603b69362228fd1b112f9d330eaff411d
Steve Dolman <stephendolman2112@gmail.com>
2019-07-01 16:06:14 +0100

    Buff: Sometimes draw on entry.

--- src/cards/ZapMagnifier.java
+++ src/cards/ZapMagnifier.java
@@ -1,5 +1,7 @@
 package cards;
 
+import java.util.Collections;
+
 import extraData.Card;
 import game.Player;
 
@@ -12,8 +14,13 @@ public class ZapMagnifier extends Card {
 	@Override
 	public void onentry(Player self, Player opponent) {
 		self.draw();
+		if (self.grave.size()>0) {
+			Collections.shuffle(self.grave);
+			if(self.grave.get(0).getName().contains("Zap")) {
+				self.draw();
+			}
+		}
 	}
-	
 	
 	@Override
 	public void graveAbility(Player self, Player opponent) {
