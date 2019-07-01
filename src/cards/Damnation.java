8c83be08be87e5901d13ecd4edb6ec2f5849c02e
Steve Dolman <stephendolman2112@gmail.com>
2019-07-01 16:05:55 +0100

    Buff: Damnation needn't have its plays-reset functionality conditional.

--- src/cards/Damnation.java
+++ src/cards/Damnation.java
@@ -13,9 +13,7 @@ public class Damnation extends Card {
 	public void onentry(Player self, Player opponent) {
 		self.movePile(self.rfg, self.grave);
 		opponent.movePile(opponent.rfg, opponent.grave);
-		if(self.playsPerTurn<2) {
 		self.playsPerTurn = 2;
-		}
 		opponent.playsPerTurn = 2;
 		self.draw();
 	}
