c388ce91dda66921a3f5dbc9080ce33feaf49a03
Steve Dolman <sdolman@sabio.co.uk>
2019-11-28 14:08:21 +0000

    Spacing

--- src/cards/Griselbrand.java
+++ src/cards/Griselbrand.java
@@ -12,14 +12,14 @@ public class Griselbrand extends Card {
 
 	@Override
 	public void onentry(Player self, Player opponent) {
-		int counter  = 0;
-		while(self.lifeTotal>1 && self.getDeck().size()>0 && counter < 7) {
+		int counter = 0;
+		while (self.lifeTotal > 1 && self.getDeck().size() > 0 && counter < 7) {
 			self.lifeTotal--;
 			self.draw();
 			counter++;
 		}
 	}
-	
+
 	@Override
 	public void graveAbility(Player self, Player opponent) {
 	}
